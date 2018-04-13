8402d2ad6251366931fea47e7188865a19804af1
deepika-rao <35284111+deepika-rao@users.noreply.github.com>
2018-04-13 17:42:33 +0530

    Update TestCase1.java

--- test/java/tests/TestCase1.java
+++ test/java/tests/TestCase1.java
@@ -1,11 +1,13 @@
 package tests;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.Test;
 import appiumServer.AppiumServerInitialization;
 import appiumServer.CommonMethods;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import pages.MenuList;
+import pages.ServicesPage;
 
 public class TestCase1 extends AppiumServerInitialization{
 	
@@ -13,10 +15,12 @@ public class TestCase1 extends AppiumServerInitialization{
 	
 	CommonMethods commonmethods;
 	
-	MenuList homepage;
+	MenuList menulist;
+	
+	ServicesPage servicespage;
 	
 	@Test(priority=1)
-	public void launchURL()
+	public void launchURL() throws IOException
 	{
 		driver.navigate().to("http://www.naarsoft.com/");
 		
@@ -24,34 +28,68 @@ public class TestCase1 extends AppiumServerInitialization{
 		
 		commonmethods = new CommonMethods(driver);
 		
-		commonmethods.log("Launch URL");		 
+		commonmethods.log("Launch URL");
+		
+		commonmethods.captureScreenShots();
 	}
 	
 	@Test(priority=2)
-	public void NavigateToAboutUSPage() throws InterruptedException 
+	public void NavigateToAboutUSPage() throws InterruptedException, IOException 
 	{
 		commonmethods = new CommonMethods(driver);
 		
-		homepage = new MenuList(driver);
+		menulist = new MenuList(driver);
 		
-		homepage.ClickAboutUs();
+		menulist.ClickAboutUs();
 		
 		Thread.sleep(2000);
 		
 		commonmethods.log("Click on AboutUS");	
+		
+		commonmethods.captureScreenShots();
 	}
 	
-	@Test(priority=3)
+	@Test(priority=4)
 	public void NavigateToHome() throws InterruptedException
 	{
 		commonmethods = new CommonMethods(driver);
 		
-		homepage = new MenuList(driver);
+		menulist = new MenuList(driver);
 		
-		homepage.ClickHome();
+		menulist.ClickHome();
 		
 		Thread.sleep(2000);
 		
 		commonmethods.log("Click on Home");
 	}
+	
+	@Test(priority=3)
+	public void NavigateToContactUS() throws InterruptedException
+	{
+		commonmethods = new CommonMethods(driver);
+		
+		menulist = new MenuList(driver);
+		
+		menulist.ClickContactUs();
+		
+		Thread.sleep(2000);
+		
+		commonmethods.log("Click on ContactUS");
+		
+	}
+	
+	@Test(priority=5)
+	public void NavigateToMicrosoft_Net() throws Exception
+	{
+		commonmethods = new CommonMethods(driver);
+		
+		servicespage = new ServicesPage(driver);
+		
+		servicespage.ClickServices_MicrosoftNet();
+		
+		Thread.sleep(2000);
+		
+		commonmethods.log("Click on Microsoft .Net");
+	}
+	
 }
